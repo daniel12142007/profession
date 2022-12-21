e771266d3e8b9feb593393e179218bb68bb47d9b
daniel12142007 <danijelahatzanov@gmail.com>
2022-12-21 13:08:55 +0600

    commit miner

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import People.Miner;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        Miner miners = new Miner();
+        System.out.println("1)Registration\n2)Deletion by ID\n3)Delete everything\n4)Get data");
+            int b = scanner.nextInt();
+            miners.Function(b);
     }
 }
--- src/People/Miner.java
+++ src/People/Miner.java
@@ -1,4 +1,54 @@
 package People;
 
-public class Miner {
+import Abstract.Person;
+import Interface.Inter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Miner extends Person implements Inter {
+    public Miner(String name, int age) {
+        super(name, age);
+    }
+    public Miner(){
+        super();
+    }
+
+    @Override
+    public int Function(int b) {
+        Scanner an = new Scanner(System.in);
+        Scanner an1 = new Scanner(System.in);
+        List<Miner> miners = new ArrayList<>();
+            if (b == 1) {
+                System.out.println("Enter name");
+                String name = an1.nextLine();
+                System.out.println("Enter age");
+                int age = an.nextInt();
+                Miner miner = new Miner(name, age);
+                miners.add(miner);
+                System.out.println("1)Registration\n2)Deletion by ID\n3)Delete everything\n4)Get data");
+                b = an.nextInt();
+            }
+            if (b == 2) {
+                System.out.println("Select ID to delete");
+                b = an.nextInt();
+                System.out.println("removed by ID"+miners.get(b));
+                miners.remove(b);
+                System.out.println("1)Registration\n2)Deletion by ID\n3)Delete everything\n4)Get data");
+                b = an.nextInt();
+            }
+            if (b == 3) {
+                miners.removeAll(miners);
+                System.out.println("Data deleted");
+                System.out.println("1)Registration\n2)Deletion by ID\n3)Delete everything\n4)Get data");
+                b = an.nextInt();
+            }
+            if (b == 4) {
+                System.out.println("Your data:");
+                System.out.println(miners);
+                System.out.println("You miner!");
+            }
+            return b;
+        }
 }
