fd8d7120860eb6db90f5d7d1db02b9344d72cb8b
daniel12142007 <danijelahatzanov@gmail.com>
2023-01-01 17:35:34 +0600

    I add

--- src/abstract1/Person.java
+++ src/abstract1/Person.java
@@ -1,32 +1,27 @@
 package abstract1;
 
-public abstract class Person {
-    private String name;
-    private int age;
+public abstract class Person<T, V> {
+    private T name;
+    private V age;
 
-    public Person(String name, int age) throws Eror{
-        if (age<0){
-            throw new Eror("Ты не родился");
-        }if (age>100){
-            throw new Eror("Ты сдох");
-        }
+    public Person(T name, V age) {
         this.name = name;
         this.age = age;
     }
 
-    public String getName() {
+    public T getName() {
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(T name) {
         this.name = name;
     }
 
-    public int getAge() {
+    public V getAge() {
         return age;
     }
 
-    public void setAge(int age) {
+    public void setAge(V age) {
         this.age = age;
     }
 
